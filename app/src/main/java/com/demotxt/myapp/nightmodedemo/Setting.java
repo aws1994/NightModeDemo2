1a866b32381ffab0c46a9cd7ddd95d683050fd70
aws1994 <awsrhimi@gmail.com>
2017-12-21 19:58:17 +0100

    Initial commit

---
+++ app/src/main/java/com/demotxt/myapp/nightmodedemo/Setting.java
@@ -0,0 +1,45 @@
+package com.demotxt.myapp.nightmodedemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class Setting extends AppCompatActivity {
+    private Switch myswitch;
+    SharedPref sharedpref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sharedpref = new SharedPref(this);
+        if(sharedpref.loadNightModeState()==true) {
+            setTheme(R.style.darktheme);
+        }
+        else  setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        myswitch=(Switch)findViewById(R.id.myswitch);
+        if (sharedpref.loadNightModeState()==true) {
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    sharedpref.setNightModeState(true);
+                    restartApp();
+                }
+                else {
+                    sharedpref.setNightModeState(false);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp () {
+        Intent i = new Intent(getApplicationContext(),Setting.class);
+        startActivity(i);
+        finish();
+    }
+}
